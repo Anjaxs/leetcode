73276b2b7fc44cc20103b36c14fd7fbb8b836be3
anjaxs <942403648@qq.com>
2022-03-10 09:40:55 +0800

    第n个丑数，单词拆分

---
+++ dynamic_planning/139_wordBreak_test.go
@@ -0,0 +1,21 @@
+package leetcode
+
+import "testing"
+
+func TestWorkBreak(t *testing.T) {
+	var tests = []struct {
+		inputStr      string
+		inputWordDict []string
+		want          bool
+	}{
+		{"leetcode", []string{"leet", "code"}, true},
+		{"applepenapple", []string{"apple", "pen"}, true},
+		{"catsandog", []string{"cats", "dog", "sand", "and", "cat"}, false},
+		{"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab", []string{"a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa"}, false},
+	}
+	for _, test := range tests {
+		if got := wordBreak(test.inputStr, test.inputWordDict); got != test.want {
+			t.Errorf("139_wordBreak(%v, %v) = %v, expected %v", test.inputStr, test.inputWordDict, got, test.want)
+		}
+	}
+}
---
+++ dynamic_planning/264_nthUglyNumber_test.go
@@ -0,0 +1,18 @@
+package leetcode
+
+import "testing"
+
+func TestNthUglyNumber(t *testing.T) {
+	var tests = []struct {
+		input int
+		want  int
+	}{
+		{10, 12},
+		{1, 1},
+	}
+	for _, test := range tests {
+		if got := nthUglyNumber(test.input); got != test.want {
+			t.Errorf("264_nthUglyNumber(%v) = %v, expected %v", test.input, got, test.want)
+		}
+	}
+}
